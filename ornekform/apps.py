471625399074c74941fbaeaa349dd7da1bcbb6d9
serif geyik <wpram734@gmail.com>
2020-12-05 16:51:52 +0300

    form eklendi

---
+++ ornekform/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OrnekformConfig(AppConfig):
+    name = 'ornekform'
---
+++ ornekform/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect,HttpResponse
+
+
+
+def index(request):
+    
+    return render(request,"ornekform/index.html")
