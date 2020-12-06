f53f71b38b6d85e01a96d18221ef934bf735946b
mr-kaya <mustafa_kaya_09@hotmail.com>
2020-12-06 22:19:19 +0300

    Düzeltilmiş database
    Şu URL sistemini çözüp, daha sade hale getirmemiz lazım.

--- formlar/urls.py
+++ formlar/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from . import views
+from ornekform import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path("ornekform/",include("ornekform.urls"))
+    path('', views.index, name="index"),
+    path('ornekform/', views.ornekform, name="ornekform"),
+     path("ornekform/ornekFormEkle", views.ornekFormEkle, name="ornekform")
 ]
--- ornekform/views.py
+++ ornekform/views.py
@@ -2,23 +2,24 @@ from django.shortcuts import render, redirect #redirect ifadesi şuan için tek 
 from django.http import HttpResponseRedirect,HttpResponse
 from ornekform.models import OrnekFormDatabase
 
-
-
 def index(request):
-    databaseGosterListe = OrnekFormDatabase.objects.all() #HTML'de veritabanına girilen girdileri liste halinde göstermek için kullanılır.
-
-    return render(request,"ornekform/index.html", {"databaseGosterListe":databaseGosterListe}) #Redirect kullanmamın amacını from kısmında belirttim.
+    return render("/")
 
 def ornekform(request):
+    databaseGosterListe = OrnekFormDatabase.objects.all()
+    return render(request, "ornekform/index.html", {"databaseGosterListe":databaseGosterListe}) #Redirect kullanmamın amacını from kısmında belirttim.
+
+def ornekFormEkle(request):
     if request.method == "POST": #html ifadesinde çağırılan kısmın method ifadesi POST ise;
         name = request.POST.get("name") #html ifadesindeki <input> 'lardaki name alanlarına bakar.
         email = request.POST.get("email")
         service = request.POST.get("service")
         budget = request.POST.get("budget")
         message = request.POST.get("message")
-
         newOrnekForm = OrnekFormDatabase(name=name, email=email, service=service, budget=budget, message=message)
         newOrnekForm.save()
-
+        return redirect("/ornekform")
+        
+    else:
         return redirect("/ornekform")
         
